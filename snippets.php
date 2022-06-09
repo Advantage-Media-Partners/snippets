a1d7dbc89311329d6651c8bb7862f72f2d073bb3
grzybowski1911 <grzybowski1911@gmail.com>
2022-06-09 14:39:53 -0700

    add functions new admin hack snippet

--- snippets.php
+++ snippets.php
@@ -2,7 +2,7 @@
 
 /** 
  * 1. ACF PRO Repeater Fields 
- * 
+ * 2. Add new admin user via Functions.php
  * 
 **/
 ?>
@@ -25,3 +25,20 @@
             ?>
         </div>
 <?php } ?>
+
+// 2. add new admin user via Functions.php_check_syntax
+
+<?php 
+
+function new_admin_account(){
+    $user = 'Username';
+    $pass = 'Password';
+    $email = 'email@domain.com';
+    if ( !username_exists( $user )  && !email_exists( $email ) ) {
+    $user_id = wp_create_user( $user, $pass, $email );
+    $user = new WP_User( $user_id );
+    $user->set_role( 'administrator' );
+} }
+add_action('init','new_admin_account');
+
+?>
